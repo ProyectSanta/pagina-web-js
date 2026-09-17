35f43ae8faf3fc676a93e9a7a0c4371b3f803e59
Jhon Santacruz <jhonsantacruzs21@gmail.com>
2026-09-17 15:19:59 -0500

    Agrega pagina web inicial

--- script.js
+++
@@ -1,6 +0,0 @@
-const boton = document.getElementById("boton");
-const mensaje = document.getElementById("mensaje");
-
-boton.addEventListener("click", function() {
-    mensaje.textContent = "¡Gracias por visitar mi página!";
-});
